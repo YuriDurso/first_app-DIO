86dbf87c2bce5624f2cd0440d50cc8bc98b2e3d8
YuriDurso <yuridurso2@gmail.com>
2022-11-22 19:21:25 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Card from "./components/Card"
+const App = () => {
+  return (
+    <div>
+      <h1>Minha primeira aplicação com React</h1>
+      <h2>Aplicação usando Componentização e Bootstrap</h2>
+      <Card/>
+    </div>
+  );
+};
+export default App;
